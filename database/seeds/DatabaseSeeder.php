f835d3ba35d6dec4ea572257c5c3329a79a50d2c
rogersovich <dimasroger89@gmail.com>
2019-12-24 23:00:24 +0700

    bismillah lagi

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Role::create([
+            'name' => 'Admin'
+        ]);
+
+        Role::create([
+            'name' => 'Pegawai'
+        ]);
+
     }
 }
